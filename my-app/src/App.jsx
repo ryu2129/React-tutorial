85a66c4035c25c95dcfc6163e67c5c80137812a2
ryu2129 <ryu212920@gmail.com>
2022-01-10 23:47:38 +0900

    jsx -js

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,12 @@
 export const App = () => {
+  const onClickButton = () => {
+    alert();
+  }
   return (
     <>
-      <h1>こんにちは！</h1>
+      <h1>こんにちは!</h1>
       <p>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   ) 
 }
