42597840562102a6f5e7d2426f6b9c497cf3eef3
ryu2129 <ryu212920@gmail.com>
2022-01-10 23:42:56 +0900

    component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,4 @@
 import ReactDOM from "react-dom";
+import { App } from "./App"
 
-const App = () => {
-  return (
-    <div>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </div>
-  ) 
-}
 ReactDOM.render(<App />, document.getElementById("root"));
